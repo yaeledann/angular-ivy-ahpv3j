7dfaf5216a2107d071fd569dd7baaff50992fbae
yaeledann <yaeledann@gmail.com>
2020-11-05 13:56:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ahpv3j
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ahpv3j)
---
+++ src/app/Profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: [ './profile.component.css' ]
+})
+export class ProfileComponent {
+  name: string ='Dann Yaele E. Taguilaso';
+} 
